c30ad00350d1a998bdf8a2f95890bba30ada55e0
Wan Lucas <wan.lucas.90@gmail.com>
2022-05-12 19:31:08 -0300

    :bug: pequena correcao no sensitive

--- módulo1/Introdução à JavaScript e Lógica de Programação/Bloco4/conversor-de-romanos.js
+++ módulo1/Introdução à JavaScript e Lógica de Programação/Bloco4/conversor-de-romanos.js
@@ -1,21 +1,26 @@
 //o objetivo é criar uma função que receba um número romano e retorne-o convertido.
 
 function convertNumber([...number]) {
-  const reference = { 
+  const ref = { 
     'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm':1000
   };
   let convertedNumber = 0;
 
   number.forEach((c, i) => {
-    const value = reference[c.toLowerCase()];
+    const value = ref[c.toLowerCase()];
     
-    if(number.slice(i).some((c) => reference[c] > value ))
-      convertedNumber -= value
+    if( number.slice(i).some((c) => ref[c.toLowerCase()] > value ))
+      convertedNumber -= value;
     else convertedNumber += value;
   });
 
   return convertedNumber;
 }
+
+const romano = 'XIV';
+const resultado = convertNumber(romano);
+
+console.log(resultado);
 
 //a minha função percorre por cada número já convertido e verifica se há algum 
 //número com valor maior à sua direita; caso haja, o atual número é subtraído do valor total
