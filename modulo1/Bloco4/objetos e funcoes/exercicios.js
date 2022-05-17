a3cef80611b68a16b8bfb12cecc62c755e1e95c7
Wan Lucas <wan.lucas.90@gmail.com>
2022-05-17 15:52:55 -0300

    :sparkles: nova resolucao

--- modulo1/Bloco4/objetos e funcoes/exercicios.js
+++ modulo1/Bloco4/objetos e funcoes/exercicios.js
@@ -1,4 +1,6 @@
-// o exercício pede que eu crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// O exercício pede que eu crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// método otimizado
 
 function mostRepeated(arr) {
   const rep = {};
@@ -19,3 +21,17 @@ function mostRepeated(arr) {
 }
 
 console.log(mostRepeated([1,1,4,5,6,7,5,4,3,3,3,3,3]));
+
+// método conciso 
+
+function mostRepeated2(arr) {
+  const rep = new Object;
+
+  for(num of arr) rep[num] ? rep[num]++ : rep[num] = 1;
+
+  for(value in rep)
+    if(rep[value] == Math.max(...Object.values(rep))) return value;
+}
+
+console.log(mostRepeated2([3,2,1,4,4,4,4,3,3]));
+
