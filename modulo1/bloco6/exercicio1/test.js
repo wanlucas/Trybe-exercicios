2dde42df66cb1c80809cd746c06f07db02b9bdda
Wan Lucas <wan.lucas.90@gmail.com>
2022-06-08 00:01:14 -0300

    :fire: deleting unnecessary files

--- modulo1/bloco6/exercicio1/test.js
+++
@@ -1,8 +0,0 @@
-function getMichaelLastName(inputText) {
-  const words = inputText.replace(/[?!,]/g, '').split(' ');
-
-  return words.filter((word, i) => {
-    return words[i - 1] === 'Michael' && word[0] === word[0].toUpperCase();
-  });
-}
-console.log(getMichaelLastName( "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?"));
