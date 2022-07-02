f912913218069b6ce1d60c97b4e46fec5e46d058
Wan Lucas <wan.lucas.90@gmail.com>
2022-07-02 14:42:18 -0300

    :memo: about setup and teardown

---
+++ modulo2/Testes-Assíncronos/setup&teardown.test.js
@@ -0,0 +1,34 @@
+let cities = [];
+
+const addCity = (city) => {
+cities.push(city);
+};
+
+const removeCity = (city) => {
+cities = cities.filter((eachCity) => eachCity !== city);
+};
+
+//default jest Function
+beforeEach(() => {
+  cities = ['Pindamonhangaba'];
+});
+
+//default jest Function
+afterEach(() => {
+  cities = [];
+});
+
+test('Testa a função addCity utilizando o beforeEach', () => {
+  expect.assertions(3);
+  addCity('Piraporinha');
+  expect(cities).toHaveLength(2);
+  expect(cities).toContain('Pindamonhangaba');
+  expect(cities).toContain('Piraporinha');
+});
+
+test('Testa a função removeCity utilizando o beforeEach', () => {
+  expect.assertions(2);
+  removeCity('Pindamonhangaba');
+  expect(cities).not.toContain('Pindamonhangaba');
+  expect(cities).toHaveLength(0);
+});
